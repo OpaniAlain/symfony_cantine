070992454baaffbcb5569aae78aeae6c68a02ead
OpaniAlain <123356567+OpaniAlain@users.noreply.github.com>
2023-07-04 11:53:52 +0200

    add commit form

--- src/Form/MenuType.php
+++ src/Form/MenuType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Menu;
+use Doctrine\DBAL\Types\FloatType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,16 @@ class MenuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('price', FloatType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
 
